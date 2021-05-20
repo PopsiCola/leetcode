c7da35b121be2f17483b953b38e051fcf589bece
liulebin <liulebinn@163.com>
2021-05-20 23:13:38 +0800

    692. 前K个高频单词 方法一：哈希表 + 排序

---
+++ src/com/cute/leetcode/editor/cn/JuZhenZhongDeLuJingLcof.java
@@ -0,0 +1,68 @@
+//给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。 
+//
+// 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。 
+//
+// 
+//
+// 例如，在下面的 3×4 的矩阵中包含单词 "ABCCED"（单词中的字母已标出）。 
+//
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "AB
+//CCED"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：board = [["a","b"],["c","d"]], word = "abcd"
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= board.length <= 200 
+// 1 <= board[i].length <= 200 
+// board 和 word 仅由大小写英文字母组成 
+// 
+//
+// 
+//
+// 注意：本题与主站 79 题相同：https://leetcode-cn.com/problems/word-search/ 
+// Related Topics 深度优先搜索 
+// 👍 316 👎 0
+
+package com.cute.leetcode.editor.cn;
+public class JuZhenZhongDeLuJingLcof {
+    public static void main(String[] args) {
+        Solution solution = new JuZhenZhongDeLuJingLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    /*public boolean exist(char[][] board, String word) {
+        int index = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (word.charAt(index) == board[i][j]) {
+                    // 判断四周是否存在与
+
+                    index++;
+
+                }
+            }
+        }
+    }*/
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
