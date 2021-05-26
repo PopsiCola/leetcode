4611238d9b3610bbf3015fd0fdde1ae9fea3f6be
liulebin <liulebinn@163.com>
2021-05-26 20:56:12 +0800

    volatile的非原子性Demo，以及如何实现原子性

--- src/com/cute/interview/thread/VolatileDemo.java
+++ src/com/cute/interview/thread/VolatileDemo.java
@@ -1,6 +1,7 @@
 package com.cute.interview.thread;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * volatile的可见性demo
@@ -13,22 +14,60 @@ class MyData {
     public void addTo60() {
         this.number = 60;
     }
+
+    public void addPlusPlus() {
+        this.number++;
+    }
+
+    // 实现原子性
+    AtomicInteger atomicInteger = new AtomicInteger();
+    public void addAtomic() {
+        atomicInteger.getAndIncrement();
+    }
+
 }
 
-/**
- * 验证volatile的可见性
- *  假如int number = 0； number变量之前根本没有添加volatile关键字修饰
- *  控制台输出： AAA	 come in
- *              AAA	 updated number value:60
- *
- *  volatile修饰后，控制台输出：
- *              AAA	 come in
- *              AAA	 updated number value:60
- *              main	 mission is over, value is 60
- */
+
 public class VolatileDemo {
 
     public static void main(String[] args) {
+        /**
+         * volatile不保证原子性Demo
+         * 如何解决原子性：
+         *  1.加synchronize
+         *  2.使用我们的juc下的AtomicInteger
+         */
+        MyData myData = new MyData();
+        for (int i = 1; i <= 20; i++) {
+            new Thread(() -> {
+                for (int j = 1; j <= 1000; j++) {
+                    myData.addPlusPlus();
+                    myData.addAtomic();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+        while (Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+
+        System.out.println(Thread.currentThread().getName() + "\t number value is " + myData.number);
+        System.out.println(Thread.currentThread().getName() + "\t atomicInteger value is " + myData.atomicInteger);
+
+    }
+
+    /**
+     * 验证volatile的可见性
+     *  假如int number = 0； number变量之前根本没有添加volatile关键字修饰
+     *  控制台输出： AAA	 come in
+     *              AAA	 updated number value:60
+     *
+     *  volatile修饰后，控制台输出：
+     *              AAA	 come in
+     *              AAA	 updated number value:60
+     *              main	 mission is over, value is 60
+     */
+    private static void seeOkByVolatile() {
         MyData myData = new MyData();
         new Thread(() -> {
             System.out.println(Thread.currentThread().getName() + "\t come in");
