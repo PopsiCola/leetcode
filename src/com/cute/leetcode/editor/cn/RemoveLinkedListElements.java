2e2e9165239c76db5b67c29d925c5df29b818823
liulebin <liulebinn@163.com>
2021-06-05 22:20:26 +0800

    203. 移除链表元素 方法一：递归 方法二：迭代

---
+++ src/com/cute/leetcode/editor/cn/RemoveLinkedListElements.java
@@ -0,0 +1,86 @@
+//给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,6,3,4,5,6], val = 6
+//输出：[1,2,3,4,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [], val = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [7,7,7,7], val = 7
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 列表中的节点在范围 [0, 104] 内 
+// 1 <= Node.val <= 50 
+// 0 <= k <= 50 
+// 
+// Related Topics 链表 
+// 👍 635 👎 0
+
+package com.cute.leetcode.editor.cn;
+public class RemoveLinkedListElements {
+    public static void main(String[] args) {
+        Solution solution = new RemoveLinkedListElements().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        ListNode node = new ListNode(0);
+        node.next = head;
+        ListNode temp = node;
+        while (temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return node.next;
+
+        // 递归
+        /*if (head == null) {
+            return head;
+        }
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;*/
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
