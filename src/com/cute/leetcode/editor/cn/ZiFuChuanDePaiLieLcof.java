fb1711a9702e43a4053af257ad921253220718a7
liulebin <liulebinn@163.com>
2021-06-22 19:03:13 +0800

    剑指 Offer 38. 字符串的排列

---
+++ src/com/cute/leetcode/editor/cn/ZiFuChuanDePaiLieLcof.java
@@ -0,0 +1,58 @@
+//输入一个字符串，打印出该字符串中字符的所有排列。 
+//
+// 
+//
+// 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。 
+//
+// 
+//
+// 示例: 
+//
+// 输入：s = "abc"
+//输出：["abc","acb","bac","bca","cab","cba"]
+// 
+//
+// 
+//
+// 限制： 
+//
+// 1 <= s 的长度 <= 8 
+// Related Topics 回溯算法 
+// 👍 344 👎 0
+
+package com.cute.leetcode.editor.cn;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ZiFuChuanDePaiLieLcof {
+    public static void main(String[] args) {
+        Solution solution = new ZiFuChuanDePaiLieLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String[] permutation(String s) {
+        Set<String> list = new HashSet<>();
+        char[] arr = s.toCharArray();
+        boolean[] visited = new boolean[arr.length];
+        dfs(arr, "", visited, list);
+        return list.toArray(new String[0]);
+    }
+
+    public void dfs(char[] arr, String s, boolean[] visited, Set<String> list) {
+        if (s.length() == arr.length) {
+            list.add(s);
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i])
+                continue;
+            visited[i] = true;
+            dfs(arr, s + String.valueOf(arr[i]), visited, list);
+            visited[i] = false;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
